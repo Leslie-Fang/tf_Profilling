748b7aa01e4a7c4f7a4a418b332729d99f3c36d4
Leslie-Fang <Leslie-Fang@users.noreply.github.com>
2020-07-22 11:28:26 +0800

    Update pytorch_profile.py

--- pytorch_profile.py
+++ pytorch_profile.py
@@ -1,5 +1,5 @@
 with torch.autograd.profiler.profile() as prof:
   result = evaluate(args, model, tokenizer, prefix=prefix)
-  with open("fp32.prof", "w") as prof_f:
-    prof_f.write(prof.key_averages().table(sort_by="cpu_time_total"))
+with open("fp32.prof", "w") as prof_f:
+  prof_f.write(prof.key_averages().table(sort_by="cpu_time_total"))
   prof.export_chrome_trace("fp32.json")
